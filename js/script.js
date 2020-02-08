ddc3ddc33d3b2bde84c257062cc514d62950e7fa
The-Livrodjx <45701263+The-Livrodjx@users.noreply.github.com>
2020-02-08 16:01:57 -0300

    Delete script.js

--- js/script.js
+++
@@ -1,8 +0,0 @@
-function adicionar() {
-    var acoes = document.getElementById('acoes').value;
-    var lista = document.getElementById('lista').innerHTML;
-
-    document.getElementById('lista').innerHTML = lista + "<li>" + acoes + "</li>";
-    document.getElementById('acoes').value = ""
-}
-
